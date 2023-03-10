ad6ce4fbe1234fe07cb6b8fc908e0be9d3c20e06
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-03-10 17:13:27 +0300

    balance init

--- internal/storage/balance/balance_repository.go
+++ internal/storage/balance/balance_repository.go
@@ -1,1 +1,31 @@
 package balance
+
+import (
+	database "github.com/vasiliyantufev/gophermart/internal/db"
+	"github.com/vasiliyantufev/gophermart/internal/model"
+)
+
+type BalanceRepository interface {
+	GetBalance(*model.User, database.DB) error
+	WithDraw(*model.User, database.DB) error
+	WithDrawals(*model.User, database.DB) error
+}
+
+type Balance struct {
+	balance *BalanceRepository
+}
+
+func (b *Balance) GetBalance(*model.User, database.DB) error {
+
+	return nil
+}
+
+func (b *Balance) WithDraw(*model.User, database.DB) error {
+
+	return nil
+}
+
+func (b *Balance) WithDrawals(*model.User, database.DB) error {
+
+	return nil
+}
