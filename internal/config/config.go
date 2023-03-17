ea31cd97410dd822af222f27c0e274a271a06ac7
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-03-18 01:15:23 +0300

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,19 +8,21 @@ import (
 
 type Config struct {
 	Address              string    `env:"RUN_ADDRESS"`
-	DatabaseUri          string    `env:"DATABASE_URI" envDefault:"host=localhost port=5432 user=postgres password=myPassword dbname=gophermart sslmode=disable"`
+	DatabaseUri          string    `env:"DATABASE_URI"`
 	AccrualSystemAddress string    `env:"ACCRUAL_SYSTEM_ADDRESS"`
 	LogLevel             log.Level `env:"DEBUG_LEVEL" envDefault:"debug"`
 	//	RateLimit            int       `env:"RATE_LIMIT"`
 }
 
+//envDefault:"host=localhost port=5432 user=postgres password=myPassword dbname=gophermart sslmode=disable"`
+
 func New() *Config {
 
 	cfg := Config{}
 
-	flag.StringVar(&cfg.Address, "a", "localhost:8080", "Адрес и порт запуска сервиса")
-	flag.StringVar(&cfg.DatabaseUri, "d", "", "Адрес подключения к базе данных")
-	flag.StringVar(&cfg.AccrualSystemAddress, "r", "", "Адрес системы расчёта начислений")
+	flag.StringVar(&cfg.Address, "a", "127.0.0.1:8080", "Адрес и порт запуска сервиса")
+	flag.StringVar(&cfg.DatabaseUri, "d", "postgresql://localhost:5432/gophermart", "Адрес подключения к базе данных")
+	flag.StringVar(&cfg.AccrualSystemAddress, "r", "/api/orders/", "Адрес системы расчёта начислений")
 	//	flag.IntVar(&cfg.RateLimit, "l", 2, "Количество одновременно исходящих запросов на сервер")
 	flag.Parse()
 
